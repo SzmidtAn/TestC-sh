db5468b856d5ad67b5eab627874c8ed2a8322d40
anetaszmidt <a.szmidt95@gmail.com>
2021-12-11 15:22:51 +0100

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Lesson1 {
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person per1 = new Person("Piotr");
+            Console.WriteLine(per1.name);
+        }
+        
+    }
+
+
+    class Person
+    {
+       public string name = "Aneta";
+
+       public Person(string getName)
+       {
+           name = getName;
+       }
+    }
+
+
+
+}
